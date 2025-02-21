241321ebfd3d1cfcacba95ddbbeedaf14aacc5c3
crealu <cameron.miller294@gmail.com>
2025-02-20 18:20:30 -0800

    Update index.tsx
    - Create link to /chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import styles from "@/styles/home.module.css";
-import Chat from './components/Chat';
-import Splitter from './components/Splitter';
 
-export default function Home({ data }: { data: { name: string; count: number }}) {
+export default function Home() {
 	return (
 		<>
 			<title>Bunpos Chat</title>
 			<main className={styles.main}>
-				{/*<Splitter />*/}
-				<Chat />
+				<a href="/chat">Chat</a>
 			</main>
 		</>
 	)
