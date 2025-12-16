9ed390f53032d21630596b62afd8588a8362d1b7
crealu <cameron.miller294@gmail.com>
2025-12-15 22:39:40 -0800

    Update renshu/index.tsx
    - Remove faulty code that linter doesn't like

--- src/pages/renshu/index.tsx
+++ src/pages/renshu/index.tsx
@@ -1,21 +1,11 @@
 "use client";
-import styles from "@/styles/resnshu.module.css";
-import { useState } from 'react';
+import styles from "@/styles/renshu.module.css";
 
 export default function Renshu() {
-	const [cursorLocation, setCursorLocation] = useState<number>(0);
-
-	const updateLocation = (event) => {
-		setCursorLocation(event.clientX);
-	}
-	
 	return (
 		<div>
-			<div 
-				className={styles.jem_sentence}
-				onClick={(e) => updateLocation(e)}
-			>
-				{cursorLocation}
+			<div className={styles.jem_sentence}>
+				Renshu
 			</div>
 		</div>
 	)
